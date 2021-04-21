e17a5df34a2d5d89b204628fdf17dbf339235ad0
Ofobrukweta jeff <oghenerukevwejeff@gmail.com>
2021-04-21 11:56:09 +0100

    feat:added the searchbar

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,25 +1,20 @@
 import React from "react";
 import "./index.css";
 
-const Card = ({ customClass, details }) => {
+const Card = ({ children, customClass, details }) => {
   return (
     <>
-      <div className={`card-container ${customClass}`}>
-        <div className="card-upper">
-          <div className="card-title">{details?.title || "TITLE"}</div>
-          <div className="card-description">
-            <div className="card-text">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              commodi laudantium quaerat pariatur expedita nesciunt, dolorem
-              nobis maxime similique voluptate praesentium labore ad optio
-              placeat? Blanditiis quae itaque mollitia dolorem voluptatibus
-              quibusdam neque, repellendus ratione reprehenderit eum ut possimus
-              quia explicabo reiciendis porro aut necessitatibus rem aliquid!
-              Repudiandae, nemo iusto.
+      <div className="characters-section">
+        <div className="card-main-inner-container">
+          <div className="list-icon-card-main">
+            <div className="icon-container-wrapper"></div>
+            <div className="icon-container-wrapper"></div>
+            <div className="icon-container-wrapper">
+              {(true) ? (<span class="fa fa-heart checked"></span>) : (<span class="fa fa-heart"></span>)}
             </div>
           </div>
+          <div className="child-main-contain">{children}</div>
         </div>
-        <div className="item-type">{details?.type || "FILMS"}</div>
       </div>
     </>
   );
--- src/views/films/index.js
+++ src/views/films/index.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Card from "../../components/card/index";
 import "./index.css";
 
-const Films = () => {
-  return <></>;
+const FilmList = () => {
+  return (
+    <>
+       <div className="films-page">
+        <div className="film-details">
+          <div className="details-wrapper">
+            {
+              [1, 2, 3, 4, 5, 6, 7].map((index) => (
+                <Card key={index}>
+                  <div className="character-header">Films</div>
+                  <div className="character">https://starwars.com/ok.o</div>
+                  <div className="character">https://starwars.com/ok.o</div>
+                  <div className="character">https://starwars.com/ok.o</div>
+                  <div className="character">https://starwars.com/ok.o</div>
+                  <div className="character">https://starwars.com/ok.o</div>
+                </Card>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Films;
+export default FilmList;
